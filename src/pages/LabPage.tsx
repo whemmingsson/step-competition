597c2288f0503a0d003403bfd759b4640f6ebcf3
William Hemmingsson <william.hemmingsson@gmail.com>
2025-07-28 21:58:23 +0200

    Added progress bar and lab page

---
+++ src/pages/LabPage.tsx
@@ -0,0 +1,10 @@
+import { GoalProgressBarExample } from "@/components/GoalProgressBar";
+
+export const LabPage = () => {
+  return (
+    <div>
+      <h1>Components lab</h1>
+      <GoalProgressBarExample />
+    </div>
+  );
+};
