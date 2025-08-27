61ef61f055ccf279177d89293fae5b7c1a7b618b
William Hemmingsson <william.hemmingsson@gmail.com>
2025-08-27 21:20:25 +0200

    Cached user badges

--- src/services/BadgeService.ts
+++ src/services/BadgeService.ts
@@ -1,27 +1,30 @@
-import type {ServiceQueryResult} from "@/types/ServiceQueryResult.ts";
+import type { ServiceQueryResult } from "@/types/ServiceQueryResult.ts";
 import supabase from "@/supabase.ts";
+import { wrapWithCache } from "@/utils/CacheWrapper";
 
 export class BadgeService {
-    public static async getBadgesForUser(
-        userId: string
-    ): Promise<ServiceQueryResult<string[]>> {
-        //const cacheKey = `badge_service-getBadgesForUser-${userId}`;
+  public static async getBadgesForUser(
+    userId: string
+  ): Promise<ServiceQueryResult<string[]>> {
+    const cacheKey = `badge_service-getBadgesForUser-${userId}`;
 
-        const {data, error} = await supabase()
-            .from("Users_Badges")
-            .select("Badges(icon_url)")
-            .eq("user_id", userId);
+    return await wrapWithCache(cacheKey, 5, async () => {
+      const { data, error } = await supabase()
+        .from("Users_Badges")
+        .select("Badges(icon_url)")
+        .eq("user_id", userId);
 
-        if (error) {
-            return {success: false, error: error.message};
-        }
-        console.log(data);
+      if (error) {
+        return { success: false, error: error.message };
+      }
 
-        // Extract icon URLs from the nested structure
-        const iconUrls: string[] = data
-            ?.map((item: any) => item.Badges?.icon_url)
-            .filter((url: string | undefined) => url !== undefined) || [];
+      // Extract icon URLs from the nested structure
+      const iconUrls: string[] =
+        data
+          ?.map((item: any) => item.Badges?.icon_url)
+          .filter((url: string | undefined) => url !== undefined) || [];
 
-        return {success: true, data: iconUrls};
-    }
+      return { success: true, data: iconUrls };
+    });
+  }
 }
